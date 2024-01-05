c6e450dba134b6b4487a00dffcd58ff964119233
Roger Lo <Roger-KF.Lo@NANSHAN.com.tw>
2024-01-05 14:49:51 +0800

    [feature] HttpClient 使用加密連線

--- src/test/java/com/nanshan/springbootresttemplate/ApacheHttpClientTest.java
+++ src/test/java/com/nanshan/springbootresttemplate/ApacheHttpClientTest.java
@@ -5,14 +5,24 @@ import com.nanshan.springbootresttemplate.utils.restful.ApiUriConfig;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.text.MessageFormat;
 
 /**
@@ -24,8 +34,8 @@ public class ApacheHttpClientTest extends BaseTest {
     @Test
     @DisplayName("[Test_001]: 測試 Apache Http Client 套件發送請求: JSON_PLACEHOLDER")
     @Disabled
-    // ref. https://www.tutorialspoint.com/apache_httpclient/apache_httpclient_http_get_request.htm
-    // ref. Setting the TLS Version Statically: https://www.baeldung.com/apache-httpclient-tls
+        // ref. https://www.tutorialspoint.com/apache_httpclient/apache_httpclient_http_get_request.htm
+        // ref. Setting the TLS Version Statically: https://www.baeldung.com/apache-httpclient-tls
     void test_001() throws IOException {
 
         System.out.println(">>> port: " + port);
@@ -51,6 +61,42 @@ public class ApacheHttpClientTest extends BaseTest {
             String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
             System.out.println(" >>> result users:  " + result);
         }
+    }
+
+    @Test
+    @DisplayName("[Test_003]: 測試 Apache Http Client 套件發送請求- SSLConnectionSocketFactory (使用 TLS): JSON_PLACEHOLDER")
+    @Disabled
+    void Test_003() throws IOException {
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                SSLContexts.createDefault(),
+                new String[]{ "TLSv1.2", "TLSv1.3" },
+                null,
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+
+        try (CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+             CloseableHttpResponse response = httpClient.execute(new HttpGet(ApiUriConfig.JSON_PLACEHOLDER.getUriInfo() + "/users/2"))
+        ) {
+            String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+            System.out.println(" >>> result users:  " + result);
+        }
+    }
+
+    @Test
+    @DisplayName("[Test_004]: 測試 Apache Http Client 套件發送請求 - HttpClientConnectionManager (使用 TLS): JSON_PLACEHOLDER")
+    @Disabled
+    void Test_004() throws NoSuchAlgorithmException, IOException {
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", new SSLConnectionSocketFactory(SSLContext.getDefault()))
+                .register("http", new PlainConnectionSocketFactory())
+                .build();
+
+        try (PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+                CloseableHttpClient httpClient = HttpClientBuilder.create().setConnectionManager(connectionManager).build();
+                CloseableHttpResponse response = httpClient.execute(new HttpGet(ApiUriConfig.JSON_PLACEHOLDER.getUriInfo() + "/users/2"))) {
+            String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+            System.out.println(" >>> result users:  " + result);
+        }
+
     }
 
 }
